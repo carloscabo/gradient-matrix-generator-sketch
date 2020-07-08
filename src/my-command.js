adddadfd150edaa6530186ea2228cfe44fa4bc98
CarlosCabo <carloscabo@gmail.com>
2020-07-08 19:27:43 +0200

    update

--- src/my-command.js
+++ src/my-command.js
@@ -5,46 +5,75 @@ const { randomEmoji } = require('./shared');
 
 export default function () {
   const doc = sketch.getSelectedDocument()
-  const selected_layers = doc.selectedLayers;
-  const selected_count = selected_layers.length;
+  var selected_layers = doc.selectedLayers;
+  var selected_count = selected_layers.length;
   const page = doc.selectedPage;
 
-  // const grad_list = ['#1458EA', '#1CE1AC', '#ff7d50', '#ff007f', '#8338EC', '#FFBE0B'];
-  let grad_list = [];
+  if (selected_count === 0) {
+    sketch.UI.message(`No layers are selected.${randomEmoji()}`);
+    throw '';
+  } else if (selected_count < 2) {
+    sketch.UI.message(`You need at least 2 color filed layers!`);
+    throw '';
+  }
 
-  selected_layers.forEach(function (layer, i) {
-    console.log((i + 1) + '. ' + layer.style.fills[0].color);
-    grad_list.push(layer.style.fills[0].color);
-  });
-
-  // Colors
+  // Sort layers by x
+  var layers = Array.from(selected_layers.layers);
+  layers.sort((a, b) => b.frame.x < a.frame.x);
+  // console.log(layers);
 
   // Swatch
-  const swatch = {};
-  swatch.w = 50;
-  swatch.h = 35;
-  swatch.offset = {};
-  swatch.offset.x = 0;
-  swatch.offset.y = 0;
-  swatch.spacing = 0;
+  const swatch = {
+    w: 50,
+    h: 35,
+    offset: {
+      x: 0,
+      y: 0
+    },
+    spacing: 0
+  };
   const swatches = [];
 
+  // const colors = ['#1458EA', '#1CE1AC', '#ff7d50', '#ff007f', '#8338EC', '#FFBE0B'];
+  let colors = [];
+
+  layers.forEach(function (layer, i) {
+    // console.log((i + 1) + '. ' + layer.style.fills[0].color);
+    colors.push(layer.style.fills[0].color);
+    if (i == 0) {
+      swatch.offset.x = layer.frame.x;
+      swatch.offset.y = layer.frame.y;
+      swatch.w = layer.frame.width;
+      swatch.h = layer.frame.height;
+    }
+    if (i == 1) {
+      swatch.spacing = layer.frame.x - (swatch.offset.x + swatch.w);
+      swatch.offset.y = layer.frame.y + swatch.spacing;
+    }
+  });
+
+  console.log(swatch);
+  // throw 'swatch';
+
   // Artboard
-  const artb_w = swatch.offset.x + ((grad_list.length - 1) * swatch.spacing) + (grad_list.length * swatch.w);
-  const artb_h = swatch.offset.y + ((grad_list.length - 1) * swatch.spacing) + (grad_list.length * swatch.hopen);
+  const artb_x = swatch.offset.x;
+  const artb_y = swatch.offset.y + swatch.h;
+  const artb_w = ((colors.length - 1) * swatch.spacing) + (colors.length * swatch.w);
+  const artb_h = ((colors.length - 1) * swatch.spacing) + (colors.length * swatch.h);
   let my_artboard = new sketch.Artboard({
+    name: 'gradient-matrix-generator-result',
     parent: page,
-    frame: { x: 0, y: 0, width: artb_w, height: artb_h },
+    frame: { x: artb_x, y: artb_y, width: artb_w, height: artb_h },
     flowStartPoint: true,
   });
-  console.log(my_artboard);
+  // console.log(my_artboard);
 
-  for (let i = 0; i < grad_list.length; i++) {
-    const color_row = grad_list[i];
-    const swatch_y = swatch.offset.y + i * swatch.h;
-    for (let j = 0; j < grad_list.length; j++) {
-      const color_column = grad_list[j];
-      const swatch_x = swatch.offset.x + j * swatch.w;
+  for (let i = 0; i < colors.length; i++) {
+    const color_row = colors[i];
+    const swatch_y = i * (swatch.h + swatch.spacing);
+    for (let j = 0; j < colors.length; j++) {
+      const color_column = colors[j];
+      const swatch_x = j * (swatch.w + swatch.spacing);
 
       var my_style = new sketch.Style()
       my_style.fills = [color_column];
@@ -54,7 +83,7 @@ export default function () {
         parent: my_artboard,
         frame: { x: swatch_x, y: swatch_y, width: swatch.w, height: swatch.h },
         style: my_style
-        // style: { fills: ['#35E6C9'] }
+        //style: { fills: ['#35E6C9'] }
       });
       swatches.push(my_square);
     }
