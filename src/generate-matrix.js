b4d7bf7e01e887d8d841e3f21f487177fe92a5dd
CarlosCabo <carloscabo@gmail.com>
2020-07-09 15:02:37 +0200

    Update test file and clean-up

--- src/generate-matrix.js
+++ src/generate-matrix.js
@@ -1,30 +1,32 @@
 import sketch from 'sketch';
 
-// Importing from custom
-const { randomEmoji } = require('./shared');
-
 export default function () {
+
+  // Skecth objects alias
   const doc = sketch.getSelectedDocument()
   const page = doc.selectedPage;
   const Style = sketch.Style;
 
-  var selected_layers = doc.selectedLayers;
-  var selected_count = selected_layers.length;
+  // Get layers
+  const selected_layers = doc.selectedLayers;
+  const selected_count = selected_layers.length;
 
+  // Check for minimun requirements
   if (selected_count === 0) {
-    sketch.UI.message(`⚠️ No layers are selected!.`);
-    throw '';
+    let msg = '⚠️ No layers are selected!.';
+    sketch.UI.message(msg);
+    throw msg;
   } else if (selected_count < 2) {
-    sketch.UI.message(`⚠️ You need at least 2 color filed layers!`);
-    throw '';
+    let msg = '⚠️ You need at least 2 color filed layers!';
+    sketch.UI.message(msg);
+    throw msg;
   }
 
-  // Sort layers by x
+  // Create copy of layers and sort by x position
   var layers = Array.from(selected_layers.layers);
   layers.sort((a, b) => b.frame.x < a.frame.x);
-  // console.log(layers);
 
-  // Swatch
+  // Base swatch object
   const swatch = {
     w: 50,
     h: 35,
@@ -34,43 +36,47 @@ export default function () {
     },
     spacing: 0
   };
-  const swatches = [];
 
-  let colors = [];
+  // Base artboard object
+  const artb = {
+    x: 0,
+    y: 0,
+    w: 1024,
+    h: 1024
+  }
 
+  // Get base positions and colors from layers
+  const colors = [];
   layers.forEach(function (layer, i) {
     // console.log((i + 1) + '. ' + layer.style.fills[0].color);
     colors.push(layer.style.fills[0].color);
+
     if (i == 0) {
-      swatch.offset.x = layer.frame.x;
-      swatch.offset.y = layer.frame.y;
       swatch.w = layer.frame.width;
       swatch.h = layer.frame.height;
+      artb.x = layer.frame.x;
+      artb.y = layer.frame.y + swatch.h;
     }
     if (i == 1) {
-      swatch.spacing = layer.frame.x - (swatch.offset.x + swatch.w);
-      swatch.offset.y = layer.frame.y + swatch.spacing;
+      swatch.spacing = layer.frame.x - (artb.x + swatch.w);
+      artb.y += swatch.spacing;
     }
   });
 
-  console.log(swatch);
-  // throw 'swatch';
-
   // Artboard
-  const artb_x = swatch.offset.x;
-  const artb_y = swatch.offset.y + swatch.h;
-  const artb_w = ((colors.length - 1) * swatch.spacing) + (colors.length * swatch.w);
-  const artb_h = ((colors.length - 1) * swatch.spacing) + (colors.length * swatch.h);
+  artb.w = ((colors.length - 1) * swatch.spacing) + (colors.length * swatch.w);
+  artb.h = ((colors.length - 1) * swatch.spacing) + (colors.length * swatch.h);
   let my_artboard = new sketch.Artboard({
     name: 'gradient-matrix-generator-result',
     parent: page,
-    frame: { x: artb_x, y: artb_y, width: artb_w, height: artb_h },
+    frame: { x: artb.x, y: artb.y, width: artb.w, height: artb.h },
     flowStartPoint: true,
   });
   // console.log(my_artboard);
 
   for (let i = 0; i < colors.length; i++) {
     const swatch_y = i * (swatch.h + swatch.spacing);
+
     const color_row = colors[i];
 
     for (let j = 0; j < colors.length; j++) {
@@ -102,10 +108,6 @@ export default function () {
                 position: 0,
                 color: color_row,
               },
-              // {
-              //   position: 0.5,
-              //   color: '#0ff1ce',
-              // },
               {
                 position: 1,
                 color: color_column,
@@ -116,12 +118,11 @@ export default function () {
       ];
 
       let my_square = new sketch.ShapePath({
+        name: `gmg-swatch-x${j + 1}-y-${i + 1}`,
         parent: my_artboard,
         frame: { x: swatch_x, y: swatch_y, width: swatch.w, height: swatch.h },
-        style: my_style
-        //style: { fills: ['#35E6C9'] }
+        style: my_style //style: { fills: ['#35E6C9'] }
       });
-      swatches.push(my_square);
     }
   }
 }
